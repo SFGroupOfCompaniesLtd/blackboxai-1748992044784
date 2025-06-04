b1b7d3944067d66d7e0d8460c5fc81f335f1a3a2
SFGroupOfCompaniesLtd <it@sfgroup.co.tz>
2025-06-04 03:18:52 +0300

    Add customer-ntaresoft/AKO-Group-V18/inventory_stock_movement/models/stock_movement_filter_wizard.py 1748996331034

---
+++ customer-ntaresoft/AKO-Group-V18/inventory_stock_movement/models/stock_movement_filter_wizard.py
@@ -0,0 +1,5 @@
+from odoo import models, fields
+
+class StockMovementFilterWizard(models.TransientModel):
+    _name = 'stock.movement.filter.wizard'
+    _description = 'Stock Movement Filter Wizard'
